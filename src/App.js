313c670da217f87cea9d26ffb7178f82b099eb7a
Melodix <melodix.commercial@gmail.com>
2024-10-12 22:50:21 +0500

    add test

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="screen">
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+        <div className="others" />
+      </div>
+      <div className="navbar"></div>
+    </div>
+  );
+}
+
+export default App;
